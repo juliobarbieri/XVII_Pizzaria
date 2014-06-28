7247e00690d97b09185784cca119e424eb18fbb4
Julio <juliobarbieri@yahoo.com.br>
2014-06-28 18:20:01 -0300

    Script do novo banco de dados

---
+++ pizzaria/sql/novocreatetable.sql
@@ -0,0 +1,23 @@
+create table cliente (telefone varchar primary key, nome varchar, endereco varchar);
+create table cardapio (nome_pizza varchar primary key, ingredientes varchar, preco varchar);
+
+create table item (
+	id serial,
+	nome_pizza varchar,
+	quantidade int,
+	constraint pk primary key (id), 
+	constraint fk_cardapio foreign key (nome_pizza) references cardapio(nome_pizza));
+
+create table pedido (
+	id serial
+	telefone varchar, 
+	data_hora timestamp, 
+	constraint pk primary key (id), 
+	constraint fk_cliente foreign key (telefone) references cliente (telefone));
+
+create table pedido_has_item (
+	id serial,
+	pedido_id int,
+	item_id int,
+	constraint pk primary key (id),
+	constraint fk_pedido );
