8bc1a870a1441cb9b852052fc352515d919486ae
Julio <juliobarbieri@yahoo.com.br>
2014-06-28 22:38:10 -0300

    Adicionando estrutura para alterar o banco posteriormente

--- pizzaria/src/br/com/pizzariadomanolo/entidades/Item.java
+++ pizzaria/src/br/com/pizzariadomanolo/entidades/Item.java
@@ -1,9 +1,19 @@
 package br.com.pizzariadomanolo.entidades;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Item {
 	
-	Pizza pizza;
-	int quantidade;
+	private Integer id;
+	private Pizza pizza;
+	private Integer quantidade;
+	
+	public int getId() {
+		return id;
+	}
 	
 	public int getQuantidade(){
 		return quantidade;
@@ -12,5 +22,40 @@ public class Item {
 	public double getValor(){
 		return pizza.getPreco();
 	}
+	
+	public void criaItem(Pizza pizza, int quantidade) {
+		//this.id = id;
+		this.pizza = pizza;
+		this.quantidade = quantidade;
+
+	}
+	
+	public void clear() {
+		this.id = null;
+		this.pizza = null;
+		this.quantidade = null;
+
+	}
+	
+	public boolean cadastrarItem() {
+		Connection conexao;
+		PreparedStatement comandoSQL;
+		
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+			conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pizza", "postgres", "postgres");
+			comandoSQL = conexao.prepareStatement("INSERT INTO ITEM VALUES(?, ?)");
+			comandoSQL.setString(1, pizza.getNomePizza());
+			comandoSQL.setInt(2, quantidade);
+			comandoSQL.executeUpdate();
+			
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			return false;
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
+	}
+	
 
 }
--- pizzaria/src/br/com/pizzariadomanolo/entidades/Pedido.java
+++ pizzaria/src/br/com/pizzariadomanolo/entidades/Pedido.java
@@ -5,13 +5,21 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 public class Pedido {
-
+	
+	private Integer id;
 	private String telefone;
 	private Date data;
 	private String nomePizza;
 	private Integer quantidade;
+	
+	private List<Item> itens;
+	
+	public Integer getId() {
+		return id;
+	}
 	
 	public String getTelefone() {
 		return telefone;
@@ -29,6 +37,10 @@ public class Pedido {
 		return quantidade;
 	}
 	
+	public List<Item> getItens() {
+		return itens;
+	}
+	
 	public void criaPedido(String telefone, String nomePizza, int quantidade) {
 		this.telefone = telefone;
 		this.nomePizza = nomePizza;
@@ -41,6 +53,34 @@ public class Pedido {
 		this.nomePizza = null;
 		this.quantidade = null;
 
+	}
+	
+	public void adicionarItem(Item item) {
+		itens.add(item);
+	}
+
+	public boolean novoCadastrarPedido() {
+		Connection conexao;
+		PreparedStatement comandoSQL;
+		
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+			conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pizza", "postgres", "postgres");
+			
+			for (Item item : itens) {
+				item.cadastrarItem();
+			}
+			
+			comandoSQL = conexao.prepareStatement("INSERT INTO PEDIDO VALUES(?, current_timestamp)");
+			comandoSQL.setString(1, telefone);
+			comandoSQL.executeUpdate();
+			
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			return false;
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean cadastrarPedido() {
