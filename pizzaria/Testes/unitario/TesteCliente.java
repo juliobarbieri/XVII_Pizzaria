ba9a0ea567b270a39d77f7f1812209a6807dab9d
Marcos Azeredo <cabral.mpa@gmail.com>
2014-07-02 10:56:49 -0300

    Adicionada a SourceFolder Testes que conterá os testes do sistema e a biblioteca junit

---
+++ pizzaria/Testes/unitario/TesteCliente.java
@@ -0,0 +1,52 @@
+package unitario;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.pizzariadomanolo.entidades.Cliente;
+
+public class TesteCliente {
+	Cliente cliente;
+	
+	@Before
+	public void instanciar(){
+		cliente = new Cliente();
+		assertNotNull(cliente);
+	}
+	
+	@Test
+	public void getTelefoneTest(){
+		assertNull(cliente.getTelefone());	
+	}
+
+	@Test
+	public void getNomeTest(){
+		assertNull(cliente.getNome());	
+	}
+
+	@Test
+	public void getEndereco(){
+		assertNull(cliente.getEndereco());	
+	}
+
+	@Test
+	public void criaClienteTest() {
+		cliente.criaCliente("nome", "telefone", "endereco");
+		assertEquals(cliente.getNome(),"nome");
+		assertEquals(cliente.getTelefone(),"telefone");
+		assertEquals(cliente.getEndereco(),"endereco");
+	}
+
+	@Test
+	public void clearTest(){
+		cliente.clear();
+		assertNull(cliente.getNome());
+		assertNull(cliente.getTelefone());
+		assertNull(cliente.getEndereco());
+	}
+
+
+
+}
