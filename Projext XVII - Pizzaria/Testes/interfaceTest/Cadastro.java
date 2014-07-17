f414ea2daa6737b59b97a56ede79e5d5d13e476c
Marcos Azeredo <cabral.mpa@gmail.com>
2014-07-16 22:54:13 -0300

    adicionando Teste

---
+++ Projext XVII - Pizzaria/Testes/interfaceTest/Cadastro.java
@@ -0,0 +1,27 @@
+package interfaceTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Cadastro {
+	
+	@Test
+	public void entrarCadastro(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://localhost/pizzariadomanolo/index.jsp");
+		
+		driver.findElement(By.linkText("Cadastro")).click();
+				
+		String inicial = driver.findElement(By.linkText("Página inicial")).getText();
+		
+		assertEquals("Página inicial",inicial);
+		
+		
+	}
+
+}
